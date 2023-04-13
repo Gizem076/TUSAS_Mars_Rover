014ff44f1d2d740f0762d1abb889d827483b116e
Gizem Ozdemir <89776999+Gizem076@users.noreply.github.com>
2023-04-13 15:58:08 +0300

    making function definitions

---
+++ mars_rover.py
@@ -0,0 +1,19 @@
+direction=['E','N','W','S'] #saatin tersi yönü
+location_move={'E':(1,0),'W':(-1, 0),'N':(0, 1),'S':(0, -1)}
+rover_number = 2
+
+def turn_right(way,direction):
+    new_way=direction[direction.index(way)-1]
+    return new_way
+
+def turn_left(way,direction):
+    new_way=direction[(direction.index(way)+1)%len(direction)]
+    return new_way
+
+def move(way,gridx,gridy,x,y,location_move):
+    new_location_x=int(x) + int(location_move[way][0])
+    new_location_y=int(y) + int(location_move[way][1])
+    if new_location_x>gridx or new_location_y>gridy:
+        print('You cannot move forwad, grid is done')
+    else:
+        return [new_location_x,new_location_y,way]
