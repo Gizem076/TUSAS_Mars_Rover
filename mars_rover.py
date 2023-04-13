1159f9f8be7517a87c81fad67243d887f7ae0630
Gizem Ozdemir <89776999+Gizem076@users.noreply.github.com>
2023-04-13 16:10:00 +0300

    Making process according to the command array.

--- mars_rover.py
+++ mars_rover.py
@@ -22,3 +22,25 @@ gridx,gridy= map(int, input('GRID:').split())
 
 all_coordinates=[] 
 final_result=[] 
+
+for i in range(1,(rover_number+1)):
+    x, y, coordinates = input('coordinates for rover %d:' %i).split()
+    x,y=int(x),int(y)
+    move_coordinates=[] 
+    move_coordinates.append([x,y,coordinates])
+    if [x,y,coordinates] not in all_coordinates:
+        all_coordinates.append([x,y,coordinates])
+        for j in input('instructions for rover %d:' %i):
+                if j not in 'RLM':
+                    print('Invalid instruction, use M or R or L and please try again')
+                    exit()
+                else:
+                    if j=='R':
+                        move_coordinates.append([move_coordinates[0][0],move_coordinates[0][1],turn_right(move_coordinates[0][2],direction)])
+                    elif j=='L':
+                        move_coordinates.append([move_coordinates[0][0],move_coordinates[0][1],turn_left(move_coordinates[0][2],direction)])
+                    else:
+                        move_coordinates.append(move(move_coordinates[0][2],gridx,gridy,move_coordinates[0][0],move_coordinates[0][1],location_move))
+                    move_coordinates.pop(0)
+    else:
+        print('The both rover is in the same location,please enter another coordinate !')
